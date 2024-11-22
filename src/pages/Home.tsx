8001414b818faa2097ba96d117859f62d1509728
belugawhalesam <sameermohd2k1@gmail.com>
2024-11-22 16:18:14 +0530

    initial-react

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './Home.css';
+import '../global.css';
+
+export const Home:React.FC = () => {
+  return (
+    <div className='home-container bg-color'>
+      <h1>Home</h1>
+    </div>
+  )
+}
+
