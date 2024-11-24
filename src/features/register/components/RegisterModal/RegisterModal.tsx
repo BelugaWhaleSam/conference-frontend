5b1e20d585d77eddd3cc9497553997a10788dd5e
belugawhalesam <sameermohd2k1@gmail.com>
2024-11-24 18:41:57 +0530

    register-step-modal

--- src/features/register/components/RegisterModal/RegisterModal.tsx
+++ src/features/register/components/RegisterModal/RegisterModal.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Modal } from '../../../../components/Modal/Modal'
+import { RegsitrationStepCounter } from '../RegisterStepCounter/RegsitrationStepCounter';
 
 import './RegisterModal.css';
+
 export const RegisterModal:React.FC = () => {
   return (
-    <div className="register-modal">
-       <Modal>
-         <h1>Register Modal</h1>
-       </Modal>
-    </div>
+    <Modal>
+      <div className="register-modal">
+         <RegsitrationStepCounter step={1}/>
+      </div>
+    </Modal>
   )
 }
