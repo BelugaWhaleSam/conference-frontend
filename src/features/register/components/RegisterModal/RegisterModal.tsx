b10688a654adab3360258381468a0f7f8dee3818
belugawhalesam <sameermohd2k1@gmail.com>
2024-11-27 13:44:43 +0530

    forms

--- src/features/register/components/RegisterModal/RegisterModal.tsx
+++ src/features/register/components/RegisterModal/RegisterModal.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Modal } from "../../../../components/Modal/Modal";
 import { RegsitrationStepCounter } from "../RegisterStepCounter/RegsitrationStepCounter";
+import { determineModalContent } from "../../utils/RegisterModalUtils";
 
 import "./RegisterModal.css";
 
@@ -15,6 +16,9 @@ export const RegisterModal: React.FC = () => {
     <Modal>
       <div className="register-modal">
         <RegsitrationStepCounter step={1} changeStep={stepButtonClicked} />
+        <div className="register-modal-content">
+          {determineModalContent(step)}
+        </div>
       </div>
     </Modal>
   );
