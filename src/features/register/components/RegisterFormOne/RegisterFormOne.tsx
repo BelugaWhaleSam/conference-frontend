6595849d7d6048237b965a14ecc0d7c54cf52900
belugawhalesam <sameermohd2k1@gmail.com>
2024-11-28 12:10:04 +0530

    form-states

--- src/features/register/components/RegisterFormOne/RegisterFormOne.tsx
+++ src/features/register/components/RegisterFormOne/RegisterFormOne.tsx
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { TextInput } from "../../../../components/TextInput/TextInput";
 
-import './RegisterFormOne.css'
+import "./RegisterFormOne.css";
+
+interface FormOneState {
+  firstName: string;
+  lastName: string;
+  email: string;
+  dateOfBirth: string;
+}
 
 export const RegisterFormOne: React.FC = () => {
+  const [stepOneState, setStepOneState] = useState<FormOneState>({
+    firstName: "",
+    lastName: "",
+    email: "",
+    dateOfBirth: "",
+  });
+
+  const updateUser = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setStepOneState({ ...stepOneState, [e.target.name]: e.target.value });
+  };
+
+  useEffect(() => {
+    console.log("State change: ", stepOneState);
+  }, [stepOneState]);
+
   return (
     <div className="reg-step-one-container">
       <div className="reg-step-one-content">
-        User input area for register 1
+        <TextInput
+          name={"firstName"}
+          label={"First"}
+          errorMessage={"Please enter your name"}
+          onChange={updateUser}
+        />
+        <TextInput
+          name={"lastName"}
+          label={"Last"}
+          errorMessage={"Please enter your name"}
+          onChange={updateUser}
+        />
+        <TextInput
+          name={"email"}
+          label={"Email"}
+          errorMessage={"Please enter a valid email"}
+          onChange={updateUser}
+        />
       </div>
     </div>
   );
